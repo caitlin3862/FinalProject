5dd991383d44a8ae6b1d067ade776b4e93cb85cf
BT_2N2_30 <sample@bths.edu>
2024-05-30 13:32:56 -0400

    added folders + song to the program

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -1,33 +1,45 @@
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-public class GraphicsPanel extends JPanel implements MouseListener {
+public class GraphicsPanel extends JPanel implements MouseListener, ActionListener {
     private BufferedImage titleBg;
+    private BufferedImage playButton;
+    private BufferedImage exitButton;
+    private BufferedImage instructionButton;
+    private Clip song;
+
 
 
 
 
     public GraphicsPanel(){
         try {
-            titleBg = ImageIO.read(new File("src/backgroundImages/disco.png"));
+            //titleBg = ImageIO.read(new File("src/homeScreenImages/holder.png"));
+            playButton = ImageIO.read(new File("src/homeScreenImages/holder.png"));
+            exitButton = ImageIO.read(new File("src/homeScreenImages/holder.png"));
+            instructionButton = ImageIO.read(new File("src/homeScreenImages/holder.png"));
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+        //startMusic();
     }
-
-
 
 
     @Override
     // controls the images on the window
     public void paintComponent(Graphics g){
         super.paintComponent(g); // add js cuz
-        g.drawImage(titleBg,0,0,null);
+        //g.drawImage(titleBg,0,0,null);
 
 
         //Graphics2D g2 = (Graphics2D) g;
@@ -56,6 +68,22 @@ public class GraphicsPanel extends JPanel implements MouseListener {
 
     @Override
     public void mouseExited(MouseEvent e) {
+
+    }
+
+    private void startMusic(){
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("src/gummyBearSong.wav").getAbsoluteFile());
+            song = AudioSystem.getClip();
+            song.open(audioInputStream);
+            song.start();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
